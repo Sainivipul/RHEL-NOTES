c435ab272aa0d18db332d9d5d02f57869102719e
Sainivipul <vipulku1213@gmail.com>
2024-06-10 18:49:20 +0530

    first-commit

---
+++ 12_Swaps.py
@@ -0,0 +1,88 @@
+# what is  swap ?
+# swap partition used by data as virtual memory when our physical memory is fully occupied 
+
+
+
+check- lsblk 
+ 
+ # CREATE SWAP PARTITION
+# to do so first we need to chage the partitin type 
+
+fdisk /dev/sdb
+t --to chnge type 
+L -- to find hax code (82 for liux swap )
+
+82 -- linux swap 
+
+w -- write ad save chage 
+
+partprobe -- save the chage 
+
+pvcretae /dev/sdb1  --physical volume 
+
+vgcreate volgroup /dev/sdb1  --volume group 
+
+lvcreate -L 2G - swapenw volgroup --logical volume 
+
+mkswap /dev/volgroup/swapnew  --make it swap 
+
+          done            
+free -m --check 
+lsblk --check we will find swap is ot in use 
+
+swapon /dev/mapper/volgroup-swapew   --activating swapenw
+
+lsblk --check
+
+blkid /dev/mapper/volgroup-swapnew --to get uu id of our swap 
+
+# to make this chage permaennt make change in fstab file 
+
+vim/etc/fsatb 
+go i last lie past UUID 
+
+UUID swap defaults 00 --etry made i fstab chage is permananet nnow 
+
+________________## EXTED THE SWAP________________
+
+swapoff /dev/dm-2(swap number )
+
+lsblk --check if swap is off 
+
+ lvextend -L XG /dev/Volgroup/swap1 --extennded the LV attached to swap 
+
+ lvs --check 
+
+ swapon /dev/mapper/volgroup-swapnew --swap is on now 
+
+ lsblk -- check 
+
+
+#suppose our data is using swap sapce for critical process but now swap is also full 
+# now we do not have eough time to create new paertiton or space to create new swap 
+# in that case we can create swap file and also if swap is absent in our machine we can also 
+# cerate swap file 
+
+_______________##Swap file ___________________
+
+# ex 64mb file swap file 
+
+#block size = 64 X 1024 = 65536
+
+login through root 
+
+dd if=/dev/zer0 of=/swapfile bs=1024 count=65535 
+
+chmod 0600 /swapfile --change permission 
+
+mkswap /swapfile --setign up swapfile
+
+
+#this will on immedeate after next boot but we want it to done right away and use
+
+swapon /swapfile
+
+vi /etc/fstab
+/swapfile swap swap defaults 0 0
+
+free -m --check 
---
+++ 14_Booting_process.py
@@ -0,0 +1,72 @@
+## Bootinng sequence of rhel 
+booting - the process of copying files of OS on RAM from HD drive is known as Booting
+booting files locate on /boot dir 
+
+
+GERNAL booting flow 
+
+POST(powe on self test)----> BIOS ( basic innput output system )------> MBR (master boot record )
+--- > GRUB2 (granunified boot loader ) --> Kernal ----> H/W installation ---> /sbin/init execution 
+-----> initrd.target execution ---> switches root filesystem ----> systemd looks for default.target 
+
+#POST (POWER ON SELF TEST)
+when we power on current flows onn motherboard and give signnal to had over process to bios 
+
+#BIOS (BASIC INPUT OUTPUT)
+basic input and output system combinationof hardware annd softeare located in chip in motherboard containing a program
+one or two beep if all good otherwise continious beep 
+here we can make limited changes which saved on shemos 
+now a days it is replaced by UEFI advanced version of bios 
+
+bios contain info of all the booting devices and handover process to MBR 
+
+#MBR (MASTER BOOT RECORD )
+64 bytes reserved for the partiton table 
+
+#GRUB 2 (GRAND UNIFIED BOOT Loder )
+
+loads kernal into memory or ram 
+provide option to choose kernal for boot ot load automatically with default one if do not get any response
+grab 2 loads its configuration from /boot/grub2/grub.cfg file 
+loads default VMLinuz kernal image then extract the contents od initramfs image
+
+#KERNAL 
+kernal try to mout root file system and this requires moduls and drivers 
+kernal get all the drivers and modules in initramfs 
+
+kernal initialises all the #H/W componenets 
+
+#/sbin/init Execution 
+kernal execute /sbin/init from the initramfs as the first process with process id (PID=1)
+in RHEL 8 systemd is teh first process and start at booting of the machine ad run  till the 
+machine run it is also the last process and loads conponenets of othger process 
+wx mounting root file system 
+
+#SWITCH ROOT FILE SYSTEM 
+kernal root flesystem switched from initramfs to system root file system 
+
+#SYTEMD LOOKS FOR DEFALT TARGET 
+systemd reads the file linked to determine the default system target 
+
+
+there are 7 targets available in RHEL 8 which can be used to boot up the machine in diffrent mode as per the requirement 
+
+ls-l /liv/systemd/system/runlevel*target -- to list all the available targets 
+
+
+------------(## difrfrent run levels )-------
+
+7 run level 0-6
+
+0-- power off run level , poweroff the system
+1-- single user mode wityhout GUI, without networking used for troubleshoot 
+2--  multiuser without GUI, without networking 
+3-- multiuser mode withour GUi but with networking 
+4-- multiuser for reserch only
+5-- multiuser graphical mode with neworking (mostly used)
+6-- reboot the system 
+
+runlevel -- shows output as previous run level and current runlevel 
+
+
+
