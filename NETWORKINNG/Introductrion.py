6bc188ccce4c69e42bda14824aaf7130425923a5
Sainivipul <vipulku1213@gmail.com>
2024-07-12 08:50:46 +0530

    first

---
+++ NETWORKINNG/Introductrion.py
@@ -0,0 +1,26 @@
+IPv4 -
+dotted decimal notaion 
+32-bytes number
+2^32 no of ip address 
+IPV6 -
+deplpyed in 1998
+128 bits number 
+hexadecimal notaion 
+2^128 numbers of IP add
+
+--------------------------------------------------------------------
+##NETWORK INTERFACE 
+A network interface is the point of interconnection between a computer and a private or public network.
+A network interface is generally a network interface card (NIC), but does not have to have a physical form.
+
+##LOOP BACK
+In the area of networking and computer systems, understanding the concept of a loopback address is necessary. Often known as the “localhost” or “loopback interface,”
+this interaction has a significant impact on how well computers operate.
+
+##BRIDGE NETWORK 
+A bridge in a computer network is a device used to connect multiple LANs together with a larger Local Area Network (LAN). The mechanism of network aggregation is known as bridging. 
+The bridge is a physical or hardware device but operates at the OSI model’s data link layer and is also known as a layer of two switches
+
+##NAT 
+With NAT, all communications sent to external hosts actually contain the external IP address and port information of the NAT device instead of internal host IP addresses or port numbers. 
+NAT only translates IP addresses and ports of its internal hosts, hiding the true endpoint of an internal host on a private network.
